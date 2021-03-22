df7ecda0540cdf9470df9f0bf799d4d038da8850
Adam Tupper <adam.tupper@outlook.com>
2021-03-22 15:20:44 -0400

    Implement get_legal_moves method.

--- blooms/BloomsLogic.py
+++ blooms/BloomsLogic.py
@@ -1,5 +1,7 @@
 """A board class for the game of Blooms.
 """
+from itertools import permutations
+
 import numpy as np
 
 
@@ -34,7 +36,29 @@ class Board:
         :param player: 0 or 1 to denote the player in question.
         :return: the list of all legal moves for the given player.
         """
-        raise NotImplementedError
+        # Convert the board to a 2D representation where each index (q, r) is
+        # 1 if there is a piece on that space or 0 otherwise.
+        flattened_board = np.sum(self.board, axis=0)
+
+        # Find the list of empty spaces
+        empty_spaces = []
+        for r in range(flattened_board.shape[1]):
+            for q in range(flattened_board.shape[0]):
+                if (q + r >= self.size - 1) and (4 * self.size - 4 - q - r >= self.size - 1):
+                    # (q, r) is a valid space
+                    if flattened_board[r][q] == 0:
+                        # The space is empty
+                        empty_spaces.append((q, r))
+
+        # Generate all possible one stone moves
+        moves = []
+        moves += [[(q, r, 0), ()] for (q, r) in empty_spaces]  # All possible one stone moves of Colour 0
+        moves += [[(q, r, 1), ()] for (q, r) in empty_spaces]  # All possible one stone moves of Colour 1
+
+        # Generate all possible two stone moves
+        moves += [[(a[0], a[1], 0), (b[0], b[1], 1)] for (a, b) in permutations(empty_spaces, r=2)]
+
+        return moves
 
     def has_legal_moves(self):
         """Return True or False depending on whether there are any legal moves
