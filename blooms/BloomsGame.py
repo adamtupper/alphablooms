d0da1c1a049112f4d72bd875e6a64b76c7982349
Adam Tupper <adam.tupper@outlook.com>
2021-04-01 16:45:46 -0400

    Begin the Blooms-specific implementation of the Game class.

---
+++ blooms/BloomsGame.py
@@ -0,0 +1,121 @@
+"""Game class for Blooms.
+"""
+from scipy.special import perm
+
+from blooms.Game import Game  # TODO: Replace with AlphaZero General import
+
+from blooms.BloomsLogic import Board
+
+
+class BloomsGame(Game):
+    """This class specifies the Game class for Blooms.
+    """
+    def __init__(self, size=4, score_target=15):
+        self.size = size
+        self.score_target = score_target
+
+    def getInitBoard(self):
+        """
+        Returns:
+            startBoard: The initial game board. Note that this is not the form that
+            will be passed to the neural network, but the entire board class.
+        """
+        board = Board(self.size, self.score_target)
+        return board
+
+    def getBoardSize(self):
+        """
+        Returns:
+            (x,y): a tuple of board dimensions
+        """
+        return 2 * self.size - 1, 2 * self.size - 1
+
+    def getActionSize(self):
+        """Note that function returns the maximum number of possible actions
+        (i.e. for when the board is empty), NOT the number of valid actions for
+        a particular board state.
+
+        Returns:
+            actionSize: number of all possible actions
+        """
+        n_spaces = (3 * self.size**2) - (3 * self.size) + 1
+        n_one_stone_moves = 2 * n_spaces
+        n_two_stone_moves = perm(n_spaces, 2)
+
+        return n_one_stone_moves + n_two_stone_moves
+
+    def getNextState(self, board, player, action):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+            action: action taken by current player
+        Returns:
+            nextBoard: board after applying action
+            nextPlayer: player who plays in the next turn (should be -player)
+        """
+        if board.is_legal_move(action):
+            board.execute_move(action, player)
+
+        return board, -player
+
+    def getValidMoves(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player
+        Returns:
+            validMoves: a binary vector of length self.getActionSize(), 1 for
+                        moves that are valid from the current board and player,
+                        0 for invalid moves
+        """
+        pass
+
+    def getGameEnded(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+        Returns:
+            r: 0 if game has not ended. 1 if player won, -1 if player lost,
+               small non-zero value for draw.
+
+        """
+        pass
+
+    def getCanonicalForm(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+        Returns:
+            canonicalBoard: returns canonical form of board. The canonical form
+                            should be independent of player. For e.g. in chess,
+                            the canonical form can be chosen to be from the pov
+                            of white. When the player is white, we can return
+                            board as is. When the player is black, we can invert
+                            the colors and return the board.
+        """
+        pass
+
+    def getSymmetries(self, board, pi):
+        """
+        Input:
+            board: current board
+            pi: policy vector of size self.getActionSize()
+        Returns:
+            symmForms: a list of [(board,pi)] where each tuple is a symmetrical
+                       form of the board and the corresponding pi vector. This
+                       is used when training the neural network from examples.
+        """
+        pass
+
+    def stringRepresentation(self, board):
+        """
+        Input:
+            board: current board
+        Returns:
+            boardString: a quick conversion of board to a string format.
+                         Required by MCTS for hashing.
+        """
+        pass
---
+++ tests/test_blooms_game.py
@@ -0,0 +1,109 @@
+"""Test for the BloomsGame module.
+"""
+from .context import blooms
+
+import numpy as np
+from scipy.special import perm
+
+from blooms.BloomsGame import BloomsGame
+from blooms.BloomsGame import Board
+
+
+def test_init_default():
+    """Check that a new default game is initialised correctly.
+    """
+    game = BloomsGame()
+
+    assert game.size == 4
+    assert game.score_target == 15
+
+
+def test_init_custom():
+    """Check that a new custom game is initialised correctly.
+    """
+    game = BloomsGame(size=5, score_target=20)
+
+    assert game.size == 5
+    assert game.score_target == 20
+
+
+def test_get_init_board():
+    """Check that a new board is initialised correctly.
+    """
+    game = BloomsGame()
+    board = game.getInitBoard()
+
+    assert type(board) == Board
+
+
+def test_get_board_size():
+    """Check that the correct board size is returned.
+    """
+    game = BloomsGame()
+
+    assert game.getBoardSize() == (7, 7)
+
+
+def test_get_action_size_base4():
+    """Check that the correct action space size is returned for a base 4 board.
+    """
+    game = BloomsGame(size=4)
+    n_spaces = 37
+
+    assert game.getActionSize() == 2 * n_spaces + perm(n_spaces, 2)
+
+
+def test_get_action_size_base3():
+    """Check that the correct action space size is returned for a base 3 board.
+    """
+    game = BloomsGame(size=3)
+    n_spaces = 19
+
+    assert game.getActionSize() == 2 * n_spaces + perm(n_spaces, 2)
+
+
+def test_get_next_state_invalid_move():
+    """Check that the original (i.e. unchanged) board state is returned if an
+    invalid move is performed.
+    """
+    game = BloomsGame(size=4)
+    board = game.getInitBoard()
+
+    # Initialise the board with a stone
+    board.place_stone((6, 2), colour=1)
+
+    initial_board_array = board.get_board_3d()
+
+    # Attempt an illegal move
+    player = 1
+    move = [(6, 2, 2), ()]
+    next_board, next_player = game.getNextState(board, player=1, action=move)
+
+    post_move_board_array = next_board.get_board_3d()
+
+    assert next_player == -player
+    assert np.all(initial_board_array == post_move_board_array)
+
+
+def test_get_next_state_valid_move():
+    """Check that the board state is updated if a legal move is performed.
+    """
+    game = BloomsGame(size=4)
+    board = game.getInitBoard()
+
+    # Attempt a legal move
+    player = 1
+    move = [(6, 2, 2), ()]
+    next_board, next_player = game.getNextState(board, player, action=move)
+
+    print(next_board.board_2d)
+
+    next_board.board_2d
+    for r in range(next_board.board_2d.shape[0]):
+        for q in range(next_board.board_2d.shape[1]):
+            if (q == 6) and (r == 2):
+                assert next_board.board_2d[r, q] == 2
+            else:
+                assert next_board.board_2d[r, q] == 0
+
+    assert next_player == -player
