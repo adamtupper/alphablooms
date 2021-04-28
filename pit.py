b3c86d046808de8e9455759193eb729a7d3cd734
Adam Tupper <adam.tupper@outlook.com>
2021-04-27 21:53:35 -0400

    Add human player for playing against an agent.

--- pit.py
+++ pit.py
@@ -1,54 +1,26 @@
+"""Use this script to play manually against a Blooms agent.
+"""
+import numpy as np
+
 import Arena
 from MCTS import MCTS
-from othello.OthelloGame import OthelloGame
-from othello.OthelloPlayers import *
-from othello.pytorch.NNet import NNetWrapper as NNet
-
-
-import numpy as np
+from blooms.BloomsGame import BloomsGame
+from blooms.BloomsPlayers import *
+from blooms.pytorch.NNet import NNetWrapper as NNet
 from utils import *
 
-"""
-use this script to play any two agents against each other, or play manually with
-any agent.
-"""
+# WARNING: The game size and score target should match the chosen agent
+game = BloomsGame(size=4, score_target=15)
+human = HumanBloomsPlayer(game).play
 
-mini_othello = False  # Play in 6x6 instead of the normal 8x8.
-human_vs_cpu = True
+# WARNING: The chosen agent should match the game size and score target
+model = NNet(game)
+model.load_checkpoint('./notebooks/checkpoints_run_3', 'checkpoint_1.pth.tar')
 
-if mini_othello:
-    g = OthelloGame(6)
-else:
-    g = OthelloGame(8)
+args = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+mcts = MCTS(game, model, args)
+agent = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
 
-# all players
-rp = RandomPlayer(g).play
-gp = GreedyOthelloPlayer(g).play
-hp = HumanOthelloPlayer(g).play
+arena = Arena.Arena(agent, human, game)
 
-
-
-# nnet players
-n1 = NNet(g)
-if mini_othello:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
-else:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','8x8_100checkpoints_best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
-mcts1 = MCTS(g, n1, args1)
-n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
-
-if human_vs_cpu:
-    player2 = hp
-else:
-    n2 = NNet(g)
-    n2.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
-    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
-    mcts2 = MCTS(g, n2, args2)
-    n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
-
-    player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
-
-arena = Arena.Arena(n1p, player2, g, display=OthelloGame.display)
-
-print(arena.playGames(2, verbose=True))
+print(arena.playGames(2, verbose=True, display=False))
